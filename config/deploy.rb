181d52585b04f3bbf96a3246da32da71a071889b
Yujin Jeong <jjiny1127@gmail.com>
2016-09-29 17:33:16 +0900

    seed for capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -48,4 +48,14 @@ namespace :deploy do
         end
     end
 
+    task :seed do
+        puts "\n=== Seeding Database ===\n"
+        on primary :db do
+            within current_path do
+                with rails_env: fetch(:stage) do
+                    execute :rake, 'db:seed'
+                end
+            end
+        end
+    end
 end
